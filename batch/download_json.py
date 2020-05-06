e339c19560747fd735f60ba2fb6db6aa3379da06
Andrea Biancini <andrea.biancini@gmai.com>
2020-05-06 07:07:03 +0200

    Download JSON

--- batch/download_json.py
+++ batch/download_json.py
@@ -3,15 +3,10 @@ import json
 import csv
 import sys
 import os.path
-import threading
-import tqdm
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from urllib import parse
 from datetime import datetime, time
-from openpyxl import Workbook
-from openpyxl.styles import Font
-from openpyxl.writer.excel import save_virtual_workbook
-from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.utils import get_column_letter
 
 '''
 Scaricare i dati dal report creato appostivamente qui:
@@ -19,28 +14,28 @@ https://cqd.teams.microsoft.com/spd/#/Dashboard?language=it-IT
 '''
 
 
-def split(a, n):
-    k, m = divmod(len(a), n)
-    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+def download_call_data(t, call_id):
+    if os.path.isfile('json/{0}.json'.format(call_id)):
+        return 0
+        
+    uri = 'https://graph.microsoft.com/beta/communications/callRecords/{0}?$expand=sessions($expand=segments)'.format(call_id)
+    head = { 'Authorization': 'Bearer {0}'.format(t) }
+    r = requests.get(uri, headers=head)
+    response = r.json()
+
+    if 'error' in response:
+        print ("%s" % response)
+        return 0
+    else:
+        with open('json/{0}.json'.format(call_id), 'w') as outfile:
+            outfile.write(json.dumps(response, indent=4))
+            outfile.close()
+
+        return 1
 
 
-def download_call_data(t, call_ids):
-    for call_id in tqdm.tqdm(call_ids):
-        #print("Getting data for call id %s" % call_id)
-        if os.path.isfile('json/{0}.json'.format(call_id)):
-            continue
-
-        uri = 'https://graph.microsoft.com/beta/communications/callRecords/{0}?$expand=sessions($expand=segments)'.format(call_id)
-        head = { 'Authorization': 'Bearer {0}'.format(t) }
-        r = requests.get(uri, headers=head)
-        response = r.json()
-
-        if 'error' in response:
-            print ("%s" % response)
-        else:
-            #print ("Saved in JSON file.")
-            with open('json/{0}.json'.format(call_id), 'w') as outfile:
-                json.dump(response, outfile)
+def end_processes(futures):
+    [ proc.cancel() for proc in futures ]
 
 
 tenant_id = os.getenv('TENANTID_ENAIP', None)
@@ -77,14 +72,23 @@ with open(filename) as csv_file:
         call_id = row[0]
         call_ids.append(call_id)
 
-call_ids = list(split(call_ids, num_threads))
-threads = []
-for thid in range(num_threads):
-    th = threading.Thread(name='non-blocking', target=download_call_data, args=(t, call_ids[thid]))
-    threads.append(th)
-    th.start()
+    csv_file.close()
 
-for th in threads:
-    th.join()
+with ProcessPoolExecutor(max_workers=num_threads) as pool:
+    futures = [ pool.submit(download_call_data, t, call_id) for call_id in call_ids ]
 
-print("Script finito.")
+    kwargs = {
+        'total': len(futures),
+        'unit': 'it',
+        'unit_scale': True,
+        'leave': True
+    }
+    
+    for f in tqdm(as_completed(futures), **kwargs):
+        pass
+
+    out = 0
+    for future in futures:
+        out += future.result()
+
+print(f'Script finito, scaricati {out} files.')
