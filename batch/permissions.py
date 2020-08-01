b58f1447c21354e01b85222d629386af4c4d579a
Andrea Biancini <andrea.biancini@gmai.com>
2020-08-01 12:18:54 +0200

    Refactored

--- batch/permissions.py
+++ batch/permissions.py
@@ -2,37 +2,84 @@ import json
 import os
 import sharepy
 
-if __name__ == '__main__':
-    root_url = "https://enaiplombardia.sharepoint.com"
-    site = 'ClassediProva'
 
-    username = os.getenv('USERNAME', None)
+class Sharepoint():
+    root_url = None
+    site = None
+    s = None
+
+    def __init__(self, root_url, site, username, password):
+        self.root_url = root_url
+        self.site = site
+
+        self.s = sharepy.connect(root_url, username, password)
+        print("Connected to Sharepoint.")
+
+    def makeCall(self, url, method='POST'):
+        call_url = f'{self.root_url}/sites/{self.site}/_api/web/{url}'
+
+        if method == 'POST':
+            response = self.s.post(call_url, headers={"Accept": "application/json"})
+        elif method == 'DELETE':
+            response = self.s.delete(call_url, headers={"Accept": "application/json"})
+        else:
+            response = self.s.get(call_url, headers={"Accept": "application/json"})
+
+        if not response.content or len(response.content) == 0:
+            return None
+
+        r = json.loads(response.content)
+        
+        if 'error' in r:
+            print(f'Error during call %s.' % r['error'])
+            return None
+
+        if 'odata.error' in r:
+            print(f'Error during call %s.' % r['odata.error'])
+            return None
+        
+        return r
+
+    def getGroupIdFromName(self, name):
+        #r = self.makeCall(f'sitegroups/getbyname(\'{name}\')?$select=Id')
+        r = self.makeCall(f'sitegroups?$select=Id,Title')
+        print("%s"%r)
+        if r is None: return None
+        return r['Id']
+
+    def getRoleIdFromName(self, name):
+        r = self.makeCall(f'roledefinitions/getbyname(\'{name}\')?$select=Id')
+        if r is None: return None
+        return r['Id']
+
+    def assignPermissions(self, collection, filename, groupname, role='Lettura'):
+        groupid = self.getGroupIdFromName(groupname)
+        groupid = '26'
+        roleid = self.getRoleIdFromName(role)
+
+        self.makeCall(f'GetFolderByServerRelativeUrl(\'{collection}/{filename}\')/ListItemAllFields/breakroleinheritance(copyRoleAssignments=true, clearSubscopes=true)')
+        print("Broken role inherintance.")
+
+        self.makeCall(f'GetFolderByServerRelativeUrl(\'{collection}/{filename}\')/ListItemAllFields/roleassignments/getbyprincipalid(principalid={groupid})', method='DELETE')
+        self.makeCall(f'GetFolderByServerRelativeUrl(\'{collection}/{filename}\')/ListItemAllFields/roleassignments/addroleassignment(principalid={groupid},roledefid={roleid})')
+        print("Added role assignment.")
+
+
+if __name__ == '__main__':
+    username = os.getenv('USER_NAME', None)
     password = os.getenv('PASSWORD', None)
 
-    s = sharepy.connect(root_url, username, password)
-
+    root_url = "https://enaiplombardia.sharepoint.com"
+    site = 'ClassediProva'
+    
+    sObj = Sharepoint(root_url, site, username, password)
+    
     collection = 'Documenti condivisi'
     filename = 'Cartella di Prova'
     groupname = 'Gruppi Amministrativi'
-    role = 'Modifica'
+    role = 'Lettura'
 
-    url = f'{root_url}/sites/{site}/_api/web/GetFolderByServerRelativeUrl(\'{collection}/{filename}\')/ListItemAllFields/breakroleinheritance(copyRoleAssignments=true, clearSubscopes=true)'
-    response = s.post(url, headers={"Accept": "application/json"})
-    r = json.loads(response.content)
-    if 'error' in r:
-        print("%s" % r)
-        exit(1)
-    else:
-        print("Broken role inherintance.")
+    sObj.assignPermissions(collection, filename, groupname, role)
 
-
-    url = f'{root_url}/sites/{site}/_api/web/GetFolderByServerRelativeUrl(\'{collection}/{filename}\')/ListItemAllFields/roleassignments/addroleassignment(principalid={groupname},roledefid={role})'
-    response = s.post(url, headers={"Accept": "application/json"})
-    r = json.loads(response.content)
-    if 'error' in r:
-        print("%s" % r)
-        exit(1)
-    else:
-        print("Added role assignment.")
 
     print("Script finito.")
