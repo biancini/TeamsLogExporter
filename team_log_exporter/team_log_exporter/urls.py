135caf53fe3f44af83fe4899fac695b40b1726d5
Andrea Biancini <andrea.biancini@gmai.com>
2020-11-08 18:49:47 +0100

    Added welcome page to django

--- team_log_exporter/team_log_exporter/urls.py
+++ team_log_exporter/team_log_exporter/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('exporter/', include('exporter.urls')),
     path('batch/', include('batch.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
