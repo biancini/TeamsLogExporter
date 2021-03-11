98bc4d3c8a7f742a84592763eaa20ede8a313a08
Andrea Biancini <andrea.biancini@gmai.com>
2021-03-11 19:51:43 +0100

    Added new excel type to heroku app

--- team_log_exporter/batch/download_helper.py
+++ team_log_exporter/batch/download_helper.py
@@ -85,6 +85,125 @@ def save_virtual_workbook(workbook):
         return in_memory.getvalue()
         
 
+def sheet_registro(filename, participants, workbook):
+    worksheet = workbook.active
+    worksheet.title = 'Registro'
+    worksheet.append(['Partecipante', 'Inizio presenza', 'Fine presenza', 'Tempo di partecipazione'])
+    for cell in worksheet["1:1"]:
+        cell.font = Font(bold=True)
+    i = 1
+    for pp in participants:
+        i = i + 1
+        start = datetime.strptime(pp['start'].split('.', 1)[0], '%Y-%m-%dT%H:%M:%S')
+        end = datetime.strptime(pp['end'].split('.', 1)[0], '%Y-%m-%dT%H:%M:%S')
+        delta = (end - start).seconds
+        delta /= 60*60*24
+        worksheet.append([
+                pp['name'],
+                start,
+                end,
+                delta
+            ])
+        cell = worksheet.cell(i, 2)
+        cell.number_format = 'dd/mm/yyyy hh:mm'
+        cell = worksheet.cell(i, 3)
+        cell.number_format = 'dd/mm/yyyy hh:mm'
+        cell = worksheet.cell(i, 4)
+        cell.number_format = 'h "ore e" mm "minuti"'
+    mediumStyle = TableStyleInfo(name='TableStyleMedium2', showRowStripes=True)
+    worksheet.add_table(Table(ref=f'A1:D{i}', displayName='RegistroPresenze', tableStyleInfo=mediumStyle))
+    worksheet.sheet_view.showGridLines = False
+    report_id = os.path.basename(filename).replace('.json', '')
+    worksheet.append([f''])
+    worksheet.append([f'Report generato per il meeting con ID: {report_id}'])
+    column_widths = [30, 20, 20, 20]
+    for i, column_width in enumerate(column_widths):
+        worksheet.column_dimensions[get_column_letter(i+1)].width = column_width
+
+
+def merge_intervals(temp_tuple):
+    temp_tuple.sort(key=lambda interval: interval[0])
+    merged = [temp_tuple[0]]
+    for current in temp_tuple:
+        previous = merged[-1]
+        if current[0] <= previous[1]:
+            previous[1] = max(previous[1], current[1])
+        else:
+            merged.append(current)
+    return merged
+
+
+def sheet_partecipazione(filename, start_time, participants, workbook):
+    worksheet = workbook.create_sheet('Partecipazione')
+    titles = ['Partecipante']
+    max_interruptions = 0
+    for pp in participants:
+        if len(pp['periods']) > max_interruptions:
+            max_interruptions = len(pp['periods'])
+
+    for i in range(max_interruptions):
+        titles.append(f'Inizio {i+1}')
+        titles.append(f'Fine {i+1}') 
+
+    worksheet.append(titles)
+    for cell in worksheet["1:1"]:
+        cell.font = Font(bold=True)
+    
+    i = 1
+    for pp in participants:
+        i = i + 1
+
+        cur_start = start_time
+        row = [pp['name']]
+        for j in range(len(pp['periods'])):
+            p = pp['periods'][j]
+            row.append(p[0] - cur_start)
+            row.append(p[1] - p[0])
+            cur_start = p[1]
+
+        worksheet.append(row)
+        for ii in range(1, len(row)+1):
+            cell = worksheet.cell(i, ii)
+            cell.number_format = 'hh "ore e" mm "minuti"'
+
+    column_widths = [30]
+    for _i in range(max_interruptions*2):
+        column_widths.append(20)
+    for ii, column_width in enumerate(column_widths):
+        worksheet.column_dimensions[get_column_letter(ii+1)].width = column_width
+        
+    max_col = get_column_letter((max_interruptions*2)+1)
+    mediumStyle = TableStyleInfo(name='TableStyleMedium2', showRowStripes=True)
+    worksheet.add_table(Table(ref=f'A1:{max_col}{i}', displayName='RegistroPartecipazione', tableStyleInfo=mediumStyle))
+    worksheet.sheet_view.showGridLines = False
+    report_id = os.path.basename(filename).replace('.json', '')
+    worksheet.append([f''])
+    worksheet.append([f'Report generato per il meeting con ID: {report_id}'])
+
+    data = Reference(worksheet, min_col=2, min_row=1, max_row=i, max_col=(max_interruptions*2)+1)
+    cats = Reference(worksheet, min_col=1, min_row=2, max_row=i)
+    chart = BarChart()
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(cats)
+    chart.type = "bar"
+    chart.grouping = "stacked"
+    chart.overlap = 100
+    chart.legend = None 
+    chart.height = 20
+    chart.width = 40
+    chart.x_axis.scaling.orientation = "maxMin"
+    for ii, s in enumerate(chart.series):
+        s.graphicalProperties.line.nofill = True
+        s.graphicalProperties.line.shadow = None
+
+        if ii % 2 == 0:
+            s.graphicalProperties.noFill = True
+        else:
+            s.graphicalProperties.solidFill = "008F00"
+
+    worksheet.add_chart(chart, "A43")
+
+
 def download_generatedexcel(t, jsonFileData, report_id=None):
     name = 'Sconosciuto'
     if 'organizer' in jsonFileData and jsonFileData['organizer'] is not None:
@@ -149,47 +268,8 @@ def download_generatedexcel(t, jsonFileData, report_id=None):
     ###########
 
     workbook = Workbook()
-    worksheet = workbook.active
-    worksheet.title = 'Registro'
-
-    worksheet.append(['Partecipante', 'Inizio presenza', 'Fine presenza', 'Tempo di partecipazione'])
-    for cell in worksheet["1:1"]:
-        cell.font = Font(bold=True)
-
-    i = 1
-    for pp in participants:
-        i = i + 1
-
-        start = datetime.strptime(pp['start'].split('.', 1)[0], '%Y-%m-%dT%H:%M:%S')
-        end = datetime.strptime(pp['end'].split('.', 1)[0], '%Y-%m-%dT%H:%M:%S')
-
-        delta = (end - start).seconds
-        delta /= 60*60*24
-
-        worksheet.append([
-            pp['name'],
-            start,
-            end,
-            delta
-        ])
-
-        cell = worksheet.cell(i, 2)
-        cell.number_format = 'dd/mm/yyyy hh:mm'
-        cell = worksheet.cell(i, 3)
-        cell.number_format = 'dd/mm/yyyy hh:mm' 
-        cell = worksheet.cell(i, 4)
-        cell.number_format = 'h "ore e" mm "minuti"' 
-
-    mediumStyle = TableStyleInfo(name='TableStyleMedium2', showRowStripes=True)
-    worksheet.add_table(Table(ref=f'A1:D{i}', displayName='RegistroPresenze', tableStyleInfo=mediumStyle))
-    worksheet.sheet_view.showGridLines = False
-
-    column_widths = [30, 20, 20, 20]
-    for i, column_width in enumerate(column_widths):
-        worksheet.column_dimensions[get_column_letter(i+1)].width = column_width
-
-    if report_id != None:
-        worksheet.append([f''])
-        worksheet.append([f'Report generato per il meeting con ID: {report_id}'])
+    sheet_registro(report_id, participants, workbook)
+    sheet_partecipazione(report_id, start_time, participants, workbook)
+    workbook.save(filename=dest_filename)
 
     return dest_filename, save_virtual_workbook(workbook)
