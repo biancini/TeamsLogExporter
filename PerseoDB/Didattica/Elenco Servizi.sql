6af2158d686748086deb51f8d54f72be68f7e295
Andrea Biancini <andrea.biancini@gmai.com>
2022-01-05 13:00:02 +0100

    Fixed query for list of services

--- PerseoDB/Didattica/Elenco Servizi.sql
+++ PerseoDB/Didattica/Elenco Servizi.sql
@@ -1,20 +1,30 @@
-SELECT b.AnnoBando,
-    tb.TipoBando,
-    sedi.SiglaSede,
-    serv.DescrEdizione,
-    --serv.CodiceEdizione,
-    ed.OreAttivita,
-    ie.OreAssegnate,
-    iec.QuotaOraIncarico
+SELECT serv.IDedizione,
+  b.AnnoBando,
+  tb.TipoBando,
+  form.TipoFormativoInterno,
+  sedi.SiglaSede,
+  serv.DescrEdizione,
+  serv.CodiceEdizione,
+  sett.TipoSettoreInt,
+  pr.CodiceProgetto,
+  pr.DescrProgetto,
+  b.CodiceBando,
+  pr.DataAvvioProg,
+  pr.DataFineProg,
+  serv.Durata,
+  (SELECT OreAttivita FROM t_AttivitaEdizioni WHERE FK_Edizione = serv.IDedizione AND FK_TipoAttivita = 1) AS OreAula,
+  (SELECT OreAttivita FROM t_AttivitaEdizioni WHERE FK_Edizione = serv.IDedizione AND FK_TipoAttivita = 2) AS OreStage,
+  (SELECT COUNT(IDiscrizione) FROM t_Iscrizioni WHERE FK_Edizione = serv.IDedizione) AS NIscr
   
-  FROM [CSF].[dbo].[t_AttivitaEdizioni] AS ed
-  LEFT JOIN [CSF].[dbo].[t_PianoServizi] AS serv ON serv.IDedizione = ed.FK_Edizione
-  LEFT JOIN [CSF].[dbo].[t_Sedi] AS sedi ON sedi.IDsede = serv.FK_SedeEdizione
-  LEFT JOIN [CSF].[dbo].[t_Azioni] AS az ON az.IDazione = serv.FK_Azione
-  LEFT JOIN [CSF].[dbo].[t_Progetti] AS pr ON pr.IDprogetto = az.FK_Progetto
-  LEFT JOIN [CSF].[dbo].[t_bandi] AS b ON b.IDbando = pr.FK_Bando
-  LEFT JOIN [CSF].[dbo].[t_TipoBando] AS tb ON tb.IDtbando = b.FK_TipoBando
-  LEFT JOIN [CSF].[dbo].[t_IncarichiEdizioni] AS ie ON ie.FK_Attivita = ed.IDattedi
-  LEFT JOIN [CSF].[dbo].[t_IncarichiEdizioniContratti] AS iec ON iec.FK_IncaricoEdizione = ie.IDincarico
-  
-  WHERE b.AnnoBando = '2021/2022'
+FROM t_PianoServizi AS serv
+  LEFT JOIN t_Sedi AS sedi ON sedi.IDsede = serv.FK_SedeEdizione
+  LEFT JOIN t_Azioni AS az ON az.IDazione = serv.FK_Azione
+  LEFT JOIN t_Progetti AS pr ON pr.IDprogetto = az.FK_Progetto
+  LEFT JOIN t_Bandi AS b ON b.IDbando = pr.FK_Bando
+  LEFT JOIN t_TipoFormativoInterno AS form ON serv.FK_TipoFormativoInterno = form.IDtformaint
+  LEFT JOIN t_TipoBando AS tb ON tb.IDtbando = b.FK_TipoBando
+  LEFT JOIN t_TipoSettoreInterno AS sett ON serv.FK_SettoreEdizione = sett.IDtsettin
+
+WHERE b.AnnoBando IN ('2019/2020', '2020/2021', '2021/2022')
+
+ORDER BY IDazione
