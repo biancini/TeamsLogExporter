881a5fc310394732e5351d21f006409e324b4479
Andrea Biancini <andrea.biancini@gmai.com>
2022-06-18 23:51:43 +0200

    New queries

--- PerseoDB/Attività/Incarichi.sql
+++ PerseoDB/Attività/Incarichi.sql
@@ -1,9 +1,11 @@
-SELECT SiglaSede AS Sede,
+SELECT
+    (CASE WHEN MONTH(t_PianoServizi.DataAvvio)>=9 THEN (CAST(YEAR(t_PianoServizi.DataAvvio) AS VARCHAR) + '/' + CAST(YEAR(t_PianoServizi.DataAvvio) + 1 AS VARCHAR)) ELSE (CAST(YEAR(t_PianoServizi.DataAvvio) - 1 AS VARCHAR) + '/' + CAST(YEAR(t_PianoServizi.DataAvvio) AS VARCHAR)) END) AS AnnoAmm,
+    SiglaSede AS Sede,
     Cognome, Nome,
     (CASE WHEN FK_Tipo=1 THEN 'Generico' WHEN FK_Tipo=2 THEN 'Interno' ELSE 'Esterno' END) AS StrTipo,
-    CodFiscale,
-    DescrTitoloStudio,
-    DATEDIFF(year, t_Docenti.DataRec, GETDATE()) AS AnniEsperienzaEnaip,
+    --CodFiscale,
+    --DescrTitoloStudio,
+    --DATEDIFF(year, t_Docenti.DataRec, GETDATE()) AS AnniEsperienzaEnaip,
     --CodiceEdizione,
     DescrEdizione,
     --CodiceProgetto,
@@ -18,8 +20,8 @@ SELECT SiglaSede AS Sede,
     (CASE WHEN FK_Tipo<=2 THEN OreAssegnate ELSE (SELECT SUM(OreSvolgere) FROM t_IncarichiEdizioniContratti WHERE FK_IncaricoEdizione=IDincarico AND ApprovaIncarico=1) END) AS OreApprovate,
     IncaricoBloccato,
     IncaricoGenericoGratuito
-    --DescrIncarico
-
+    --DescrIncarico,
+    
 FROM t_Docenti
 INNER JOIN t_IncarichiEdizioni ON t_Docenti.IDdocente = t_IncarichiEdizioni.FK_Docente
 INNER JOIN t_PianoServizi ON t_IncarichiEdizioni.FK_Edizione = t_PianoServizi.IDedizione
@@ -31,8 +33,9 @@ INNER JOIN t_AttivitaEdizioni ON t_IncarichiEdizioni.FK_Attivita = t_AttivitaEdi
 INNER JOIN t_TipoAttivitaEdizione ON t_AttivitaEdizioni.FK_TipoAttivita = t_TipoAttivitaEdizione.IDtattivita
 LEFT OUTER JOIN t_TipoFormativoInterno ON t_PianoServizi.FK_TipoFormativoInterno = t_TipoFormativoInterno.IDtformaint
 LEFT OUTER JOIN t_ArgomentiModuli ON t_IncarichiEdizioni.FK_Argomento = t_ArgomentiModuli.IDargomento
+LEFT OUTER JOIN t_IscrittiEdizione ON t_IncarichiEdizioni.IDincarico = t_IscrittiEdizione.IDincarico
 
-WHERE AnnoBando IN ( '2019/2020', '2021/2022' )
+WHERE AnnoBando IN ( '2018/2019', '2019/2020', '2021/2022' )
 AND FK_Tipo > 1
 
 AND SedeTest=0
---
+++ PerseoDB/Ricavi/Valori PdR.sql
@@ -0,0 +1,34 @@
+SELECT 
+  (CASE WHEN MONTH(serv.DataAvvio)>=9 THEN (CAST(YEAR(serv.DataAvvio) AS VARCHAR) + '/' + CAST(YEAR(serv.DataAvvio) + 1 AS VARCHAR)) ELSE (CAST(YEAR(serv.DataAvvio) - 1 AS VARCHAR) + '/' + CAST(YEAR(serv.DataAvvio) AS VARCHAR)) END) AS AnnoAmm,
+  tb.TipoBando,
+  form.TipoFormativoInterno,
+  sedi.SiglaSede,
+  serv.DescrEdizione,
+  serv.CodiceEdizione,
+  sett.TipoSettoreInt,
+  pr.IDprogetto,
+  pr.CodiceProgetto,
+  pr.DescrProgetto,
+  tp.TipoProgetto,
+  b.CodiceBando,
+  pr.DataAvvioProg,
+  pr.DataFineProg,
+  serv.Durata,
+  (SELECT COUNT(IDiscrizione) FROM t_Iscrizioni WHERE FK_Edizione = serv.IDedizione) AS NIscr,
+  (SELECT COUNT(ImportoTotaleDote) FROM t_StudentiDoti AS doti LEFT JOIN t_Iscrizioni AS iscr ON iscr.FK_DoteStudente = doti.IDdotestud WHERE FK_Edizione = serv.IDedizione AND ImportoTotaleDote > 0) AS NumeroDoti,
+  (SELECT ROUND(SUM(ImportoTotaleDote), 2) FROM t_StudentiDoti AS doti LEFT JOIN t_Iscrizioni AS iscr ON iscr.FK_DoteStudente = doti.IDdotestud WHERE FK_Edizione = serv.IDedizione) AS ImportoDoti,
+  serv.QuotaIscr,
+  serv.QuotaInteg,
+  serv.DescrQuotaTotale
+
+FROM t_PianoServizi AS serv
+  LEFT JOIN t_Sedi AS sedi ON sedi.IDsede = serv.FK_SedeEdizione
+  LEFT JOIN t_Azioni AS az ON az.IDazione = serv.FK_Azione
+  LEFT JOIN t_Progetti AS pr ON pr.IDprogetto = az.FK_Progetto
+  LEFT JOIN t_TipoProgetto AS tp ON pr.FK_TipoProgetto = tp.IDtprogetto
+  LEFT JOIN t_Bandi AS b ON b.IDbando = pr.FK_Bando
+  LEFT JOIN t_TipoFormativoInterno AS form ON serv.FK_TipoFormativoInterno = form.IDtformaint
+  LEFT JOIN t_TipoBando AS tb ON tb.IDtbando = b.FK_TipoBando
+  LEFT JOIN t_TipoSettoreInterno AS sett ON serv.FK_SettoreEdizione = sett.IDtsettin
+
+WHERE (CASE WHEN MONTH(serv.DataAvvio)>=9 THEN (CAST(YEAR(serv.DataAvvio) AS VARCHAR) + '/' + CAST(YEAR(serv.DataAvvio) + 1 AS VARCHAR)) ELSE (CAST(YEAR(serv.DataAvvio) - 1 AS VARCHAR) + '/' + CAST(YEAR(serv.DataAvvio) AS VARCHAR)) END) IN ('2019/2020', '2020/2021')
